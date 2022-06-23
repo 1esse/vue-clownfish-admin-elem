511e1fe287c0764030c305b487ca41faec5a75d7
1esse <572116385@qq.com>
2022-06-23 17:37:23 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,8 @@ import 'virtual:svg-icons-register'
 import './permission'
 import 'element-plus/theme-chalk/el-message.css' // message样式
 
-// 只在开发环境且appConfig的mock字段为‘on’的情况启动mock
-import.meta.env.MODE === 'development' && mock === 'on' && enableMock()
+// 只在开发环境且appConfig的mock字段为‘on’的情况启动mock，
+// 由于线上预览需要，这里不限制环境
+['development', 'staging', 'production'].includes(import.meta.env.MODE) && mock === 'on' && enableMock()
 
 createApp(App).use(createPinia()).use(router).mount('#app')
