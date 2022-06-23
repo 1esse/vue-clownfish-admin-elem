649af36f5cb4c524c2b641c3585223c0756b4e93
1esse <572116385@qq.com>
2022-06-23 17:10:03 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
+import { mock } from './appConfig'
+import enableMock from './mock'
 import '@/styles/index.postcss' // 全局样式
 import 'virtual:svg-icons-register'
 import './permission'
+import 'element-plus/theme-chalk/el-message.css' // message样式
+
+// 只在开发环境且appConfig的mock字段为‘on’的情况启动mock
+import.meta.env.MODE === 'development' && mock === 'on' && enableMock()
 
 createApp(App).use(createPinia()).use(router).mount('#app')
